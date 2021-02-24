3ad15e27db81d1469df7fca82297d645717c806f
adith01 <adithjaison@gmail.com>
2021-02-24 13:08:07 +0000

    This is my second commit after blog creation

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
-# Create your views here.
 def all_blogs(request):
-    # blogs = Blog.objects.all()
+    blogsLen = Blog.objects.all()
+    length = len(blogsLen)
     blogs = Blog.objects.order_by('-date')[:2]
-    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs, 'length': length})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
